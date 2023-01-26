4e40b9685fcd54c3f824399dfee620a822267609
ruban-m <rubantamilboy@gmail.com>
2023-01-26 11:11:42 +0530

    finished

---
+++ src/main/java/dev/ruban/movies/ReviewRepository.java
@@ -0,0 +1,8 @@
+package dev.ruban.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {}
